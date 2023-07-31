efd0480941c8512b57902717d863f39b9573c670
Breno <onerbreno@outlook.com>
2023-07-31 11:50:36 -0300

    feat Add static home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
+import { Container, Header } from '../styles/pages/app'
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
+import Image from 'next/image'
+
+import logoImg from '../assets/logo.svg'
 
 globalStyles()
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
